0ca9f9cb75c608cdd126af1573fb60659d0189fa
eliseo1677 <eliseo1677@hotmail.com>
2025-10-22 23:49:27 +0000

    first commit

---
+++ src/router.tsx
@@ -0,0 +1,27 @@
+import { lazy, Suspense } from "react"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./layouts/Layout";
+
+const IndexPage = lazy(() => import("./views/IndexPage"));
+const FavoritesPage = lazy(() => import("./views/FavoritesPage"));
+
+export default function AppRouter() {
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route element={<Layout />}>
+					<Route path="/" element={
+						<Suspense fallback={<p>Cargando...</p>}>
+							<IndexPage />
+						</Suspense>
+					} index />
+					<Route path="/favoritos" element={
+						<Suspense fallback={<p>Cargando...</p>}>
+							<FavoritesPage />
+						</Suspense>
+					} />
+				</Route>
+			</Routes>
+		</BrowserRouter>
+	);
+}
